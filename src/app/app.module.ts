9a58e0b632816470ab4ecc071b5cbd08c5947f46
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2021-05-08 17:13:53 -0700

    add auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyDNlHMNttExA6IIglXSdti6gO1CO9h-85E',
+      authDomain: 'fir-angular-auth-84ce0.firebaseapp.com',
+      projectId: 'fir-angular-auth-84ce0',
+      storageBucket: 'fir-angular-auth-84ce0.appspot.com',
+      messagingSenderId: '375315477215',
+      appId: '1:375315477215:web:014bcec466a2a5acb2af6a',
+      measurementId: 'G-NERX1G953L',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
