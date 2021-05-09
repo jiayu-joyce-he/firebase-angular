057ace0b5dbde407d04137c78d73733170ebe06a
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2021-05-08 17:21:17 -0700

    hide config data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,13 @@ import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FirebaseService } from './services/firebase.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp({
-      apiKey: 'AIzaSyDNlHMNttExA6IIglXSdti6gO1CO9h-85E',
-      authDomain: 'fir-angular-auth-84ce0.firebaseapp.com',
-      projectId: 'fir-angular-auth-84ce0',
-      storageBucket: 'fir-angular-auth-84ce0.appspot.com',
-      messagingSenderId: '375315477215',
-      appId: '1:375315477215:web:014bcec466a2a5acb2af6a',
-      measurementId: 'G-NERX1G953L',
-    }),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [FirebaseService],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDNlHMNttExA6IIglXSdti6gO1CO9h-85E',
+    authDomain: 'fir-angular-auth-84ce0.firebaseapp.com',
+    projectId: 'fir-angular-auth-84ce0',
+    storageBucket: 'fir-angular-auth-84ce0.appspot.com',
+    messagingSenderId: '375315477215',
+    appId: '1:375315477215:web:014bcec466a2a5acb2af6a',
+    measurementId: 'G-NERX1G953L',
+  },
 };
 
 /*
